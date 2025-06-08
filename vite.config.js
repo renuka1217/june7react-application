4c7b5035c4d65dfc159c5d3312dc0618c26c14b0
renuka1217 <90893936+renuka1217@users.noreply.github.com>
2025-06-08 15:21:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})
-
-export default {
-  // other config...
-  server: {
+    server: {
     allowedHosts: ['renuka.com']
-  }
-};
+  },
+})
